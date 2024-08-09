b238bf10999825668fc84a4e4552386d03f705e6
Basmalaabokhaial <bm9214068@gmail.com>
2024-08-10 01:19:43 +0300

    Add files via upload

---
+++ Travel.js
@@ -0,0 +1,22 @@
+let searchBtn = document.querySelector("#search-btn")
+let searchForm = document.querySelector(".search-form") 
+let loginForm = document.querySelector(".login-form")
+let menuBar = document.querySelector("#menu-bar")
+let amenu = document.querySelector(".navbar")
+let vidBtn = document.querySelectorAll(".video-btn")
+
+function showbar(){
+    searchBtn.classList.toggle("fa-times")
+    searchForm.classList.toggle("active")
+}
+function showform(){
+    loginForm.classList.add("active")
+}
+function hideform(){
+    loginForm.classList.remove("active")
+
+}
+function showmenu(){
+    menuBar.classList.toggle("fa-times")
+    amenu.classList.toggle("active")
+}
